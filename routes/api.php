555240afc4d4d89134c909e218435cc28fe283a8
Mexriddin <mekkinuriddinov@gmail.com>
2025-02-24 17:35:20 +0500

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\GroupController;
-use App\Http\Controllers\GroupStudentController;
-use App\Http\Controllers\GroupSubjectController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\GroupController;
+use App\Http\Controllers\API\GroupStudentController;
+use App\Http\Controllers\API\GroupSubjectController;
+use App\Http\Controllers\API\RoomController;
 use App\Http\Controllers\GroupTeacherController;
-use App\Http\Controllers\RoomController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\SubjectTeacherController;
 use Illuminate\Http\Request;
